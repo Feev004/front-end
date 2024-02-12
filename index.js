0799e6f216c3413e1a4dc08d15776e49c51292ea
Feev004 <feevfeev2@gmail.com>
2024-02-12 10:43:49 +0700

    up

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const base_url = "http://10.104.7.149"
 
 
 
-app.set('views', path.join(__dirname, "/public/views"))
+//app.set('views', path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false}))
