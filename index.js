ed5666e883c49880f8ce5c22994331711433de50
Feev004 <feevfeev2@gmail.com>
2024-02-12 10:20:07 +0700

    up

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var bodyParser = require('body-parser')
 
 
 
-const base_url = "http://localhost:3000"
-//const base_url = "http://10.104.7.149"
+//const base_url = "http://localhost:3000"
+const base_url = "http://10.104.7.149"
 //const base_url = "node56377-noderest-test1.proen.app.ruk-com.cloud:11298"
 
 
